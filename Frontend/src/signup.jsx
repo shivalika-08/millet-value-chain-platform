b9e7c32272f0405fdd2bb1c68eaeb59e9e58e9a7
LOVE KESH SHARMA <156709375+lovekesh7890@users.noreply.github.com>
2026-04-20 14:00:58 +0000

    axios

--- Frontend/src/signup.jsx
+++ Frontend/src/signup.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import "./Registration.css";
+
 
 const Signup = () => {
   const navigate = useNavigate();
