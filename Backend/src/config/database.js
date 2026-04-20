3571436a0ed302a15ef878739949dfe4c54eb5fb
LOVE KESH SHARMA <156709375+lovekesh7890@users.noreply.github.com>
2026-04-20 14:05:11 +0000

    connection

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,7 +1,1 @@
 
-const mongoose = require("mongoose");
-
-const connectDB = async()=>{
-    await mongoose.connect("mongodb+srv://divyanshu983770_db_user:Zj8GwEryJbT3unza@cluster0.absepul.mongodb.net/millet")
-}
-module.exports = connectDB;
