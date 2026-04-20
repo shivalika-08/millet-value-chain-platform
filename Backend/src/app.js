6d505d87a673295214130898fe623c281a3108fa
LOVE KESH SHARMA <156709375+lovekesh7890@users.noreply.github.com>
2026-04-20 14:35:51 +0000

    change

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,7 +13,7 @@ const MY_SECRET_KEY = "1234@#$1";
 
 
 mongoose
-  .connect("mongodb+srv://lovekeshsharma1999_db_user:1IlHd4LitSacOsVD@cluster0.rp4d0m1.mongodb.net/registration?retryWrites=true&w=majority")
+  .connect("mongodb+srv://lovekeshsharma1999_db_user:Love%7890@cluster0.rp4d0m1.mongodb.net/?appName=Cluster0")
   .then(() => console.log("MongoDB connected "))
   .catch((err) => console.log("Connection error :", err.message));
 
